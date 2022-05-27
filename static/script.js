52b28310c9e4c2d46421f97c1059ac9cc089252d
Ilhamsyah <84080194+ilhamsyah13@users.noreply.github.com>
2022-05-27 16:52:21 +0700

    commit

--- static/script.js
+++ static/script.js
@@ -34,21 +34,13 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
   });
 });
 
-/**
- * Updates the thumbnail on a drop zone element.
- *
- * @param {HTMLElement} dropZoneElement
- * @param {File} file
- */
 function updateThumbnail(dropZoneElement, file) {
   let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
 
-  // First time - remove the prompt
   if (dropZoneElement.querySelector(".drop-zone__prompt")) {
     dropZoneElement.querySelector(".drop-zone__prompt").remove();
   }
 
-  // First time - there is no thumbnail element, so lets create it
   if (!thumbnailElement) {
     thumbnailElement = document.createElement("div");
     thumbnailElement.classList.add("drop-zone__thumb");
@@ -57,7 +49,6 @@ function updateThumbnail(dropZoneElement, file) {
 
   thumbnailElement.dataset.label = file.name;
 
-  // Show thumbnail for image files
   if (file.type.startsWith("image/")) {
     const reader = new FileReader();
 
